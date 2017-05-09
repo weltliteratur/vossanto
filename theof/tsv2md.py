045090338f1980bdd155ce6e17e78b523e2b4e91
Robert Jäschke <jaeschke@l3s.de>
2017-05-09 17:01:55 +0100

    +formatting script

---
+++ theof/tsv2md.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#
+# Reads a TSV file and reformats it into Markdown.
+#
+# Usage:
+# 
+# Author: rja 
+#
+# Changes:
+# 2017-05-09 (rja)
+# - initial version 
+
+from __future__ import print_function
+import re
+import fileinput
+
+re_articleid = re.compile(r"nyt_corpus_([0-9]{4})\.har([0-9/]+)\.xml")
+# first version
+re_theof = re.compile("(\\bthe\\s([A-Z][a-z]+\\s+){1,3}of\\b)")
+
+if __name__ == '__main__':
+    for line in fileinput.input():
+        articleid, itemid, phrase, item, sentence = line.strip().split("\t", 4)
+        # clean up the parts
+        m = re_articleid.match(articleid)
+        articleid = m.group(1) + m.group(2)
+
+        # print result
+        print("-", "[" + item + "](" + itemid + ")", "(" + articleid + ")", re_theof.sub("*\\1*", sentence))
+    
