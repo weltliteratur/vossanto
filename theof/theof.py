dbaa01be889788737d59933835b596352774d3de
Robert Jäschke <jaeschke@l3s.de>
2017-05-10 18:06:12 +0100

    refactored using generators

--- theof/theof.py
+++ theof/theof.py
@@ -10,7 +10,8 @@
 #
 # Changes:
 # 2017-05-10 (rja)
-#- smaller cleanups
+# - added parameter -r to select the regex to use (default: 4)
+# - refactored using generators
 # 2017-05-09 (rja)
 # - extended regex pattern
 # 2017-05-08 (rja)
@@ -30,22 +31,26 @@ from nltk.tokenize import sent_tokenize
 version = "0.0.2"
 
 # convert all output into a byte string to be safe when redirecting
-UTF8Writer = codecs.getwriter('utf8')
-sys.stdout = UTF8Writer(sys.stdout)
+# UTF8Writer = codecs.getwriter('utf8')
+# sys.stdout = UTF8Writer(sys.stdout)
 
 # remove line breaks and tabs from text
 re_ws = re.compile('[\n\t\r]+')
-# 1: simple match
-# re_theof = re.compile("(\\bthe\\s([A-Z][a-z]+\\s+){1,3}of\\b)")
-# 2: more than three words
-# re_theof = re.compile("(\\bthe\\s([A-Z][a-z]+\\s+)+of\\b)")
-# 3: lower-case characters and at most five words
-# re_theof = re.compile("(\\bthe\\s([A-Za-z]+\\s+){1,5}+of\\b)")
-# 4: all unicode characters
-re_theof = re.compile("(\\bthe\\s(\\w+\\s+){1,5}?of\\b)", re.UNICODE)
+re_theof = {
+    # 1: simple match
+    1: re.compile("(\\bthe\\s([A-Z][a-z]+\\s+){1,3}of\\b)"),
+    # 2: more than three words
+    2: re.compile("(\\bthe\\s([A-Z][a-z]+\\s+)+of\\b)"),
+    # 3: lower-case characters and at most five words
+    3: re.compile("(\\bthe\\s([A-Za-z]+\\s+){1,5}of\\b)"),
+    # 4: all unicode characters (+non-greedy)
+    4: re.compile("(\\bthe\\s(\\w+\\s+){1,5}?of\\b)", re.UNICODE),
+    # 5: all unicode characters and .-,'
+    5: re.compile("(\\bthe\\s+([\\w.,'-]+\\s+){1,5}?of\\b)", re.UNICODE)
+}
 
 # convert NYT XML to text
-def xml2str(f, fname):
+def xml2str(f):
     tree = ET.parse(f)
     root = tree.getroot()
     # path to the heading
@@ -53,7 +58,7 @@ def xml2str(f, fname):
     head = ""
     for block in heading:
         head = ET.tostring(block, encoding="utf-8", method="text").decode("utf-8")
-        
+
     # path to the main text block
     content = root.findall("./body/body.content/")
     body = ""
@@ -63,40 +68,62 @@ def xml2str(f, fname):
             body = body + "\n" + ET.tostring(block, encoding="utf-8", method="text").decode("utf-8")
     return head + "\n\n" + body
 
-def extract(f, fname):
-    # process contained file
-    txt = xml2str(f, fname)
-    if txt:
-        for sentence in sent_tokenize(txt):
-            # find matches
-            try:
-                for match in re_theof.findall(sentence):
-                    print(fname, re_ws.sub(' ', match[0]), re_ws.sub(' ', sentence), sep='\t')
-            except UnicodeDecodeError:
-                print(fname, "UnicodeDecodeError", file=sys.stderr)
+# convert a list of XML files into texts
+def gen_text(files):
+    for f, fname in files:
+        yield fname, xml2str(f)
+
+# extract sentences for texts
+def gen_sentences(texts):
+    for fname, text in texts:
+        for sentence in sent_tokenize(text):
+            yield fname, sentence
+
+# apply the regex
+def gen_regex(texts, regex, groupid=0):
+    for fname, text in texts:
+        for match in regex.findall(text):
+            yield fname, match[groupid], text
+
+# remove control characters
+def gen_rm_ctrl(texts):
+    for fname, match, sentence in texts:
+        yield fname, re_ws.sub(' ', match), re_ws.sub(' ', sentence)
+
+# generate a list of files from various input
+def gen_files(path):
+    if os.path.splitext(path)[1] == ".xml":
+        # XML input: just one file
+        yield open(path, "rt"), path
+    elif os.path.isdir(path):
+        # read all files in the directory
+        for fname in os.listdir(path):
+            fname = path + "/" + fname
+            if os.path.isfile(fname):
+                yield open(fname, "rt"), fname
+    else:
+        # read TAR file
+        tar = tarfile.open(path, "r:gz")
+        for tarinfo in tar:
+            if tarinfo.isreg():
+                yield tar.extractfile(tarinfo), tarinfo.name
+
+def print_matches(matches, sep='\t'):
+    for fname, match, sentence in matches:
+        print(fname, match, sentence, sep=sep)
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Find Vossantos in the NYT corpus.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('input', type=str, help='input TAR/XML file or directory')
+    parser.add_argument('-r', '--regex', type=int, help='select regex', default=4)
     parser.add_argument('-v', '--version', action="version", version="%(prog)s " + version)
 
     args = parser.parse_args()
 
-    if os.path.splitext(args.input)[1] == ".xml":
-        # XML input: just one file
-        with open(args.input, "rt") as f:
-            extract(f, args.input)
-    elif os.path.isdir(args.input):
-        # read all files in the directory
-        for fname in os.listdir(args.input):
-            fname = args.input + "/" + fname
-            if os.path.isfile(fname):
-                with open(fname, "rt") as f:
-                    extract(f, fname)
-    else:
-        # read TAR file
-        tar = tarfile.open(args.input, "r:gz")
-        for tarinfo in tar:
-            if tarinfo.isreg():
-                extract(tar.extractfile(tarinfo), tarinfo.name)
+    files = gen_files(args.input)
+    texts = gen_text(files)
+    sents = gen_sentences(texts)
+    match = gen_regex(sents, re_theof[args.regex])
+    match = gen_rm_ctrl(match)
+    print_matches(match)
