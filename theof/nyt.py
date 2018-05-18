986b7b2b7c014c25162a5fa00b2786dd9257215c
Robert Jäschke <jaeschke@l3s.de>
2018-05-18 13:32:38 +0200

    extended for url and date extraction

--- theof/nyt.py
+++ theof/nyt.py
@@ -9,6 +9,10 @@
 # Author: rja
 #
 # Changes:
+# 2018-05-18 (rja)
+# - restricted directory input to XML files
+# - fixed URL extraction
+# - added date extraction
 # 2017-08-17 (rja)
 # - made file path handling os independent
 # 2017-06-14 (rja)
@@ -26,13 +30,13 @@ import os
 import sys
 import codecs
 
-version = "0.0.3"
+version = "0.0.4"
 
 # remove line breaks and tabs from text
 re_ws = re.compile('[\n\t\r]+')
 
 # convert NYT XML to text
-def gen_parts(files, heading, text, url, category, desk, author):
+def gen_parts(files, heading, text, url, category, desk, author, date):
     for f, fname in files:
         result = [fname]
 
@@ -55,7 +59,7 @@ def gen_parts(files, heading, text, url, category, desk, author):
 
         if url:
             u = ""
-            for tag in root.findall("./head/pubdata/"):
+            for tag in root.findall("./head/pubdata"):
                 if tag.attrib["ex-ref"]:
                     u = tag.attrib["ex-ref"]
             result.append(u)
@@ -83,6 +87,27 @@ def gen_parts(files, heading, text, url, category, desk, author):
                     a = a.strip().title()
             result.append(a)
 
+        if date:
+            y = ""
+            m = ""
+            d = ""
+            for tag in root.findall("./head/meta"):
+                if tag.attrib["name"] == "publication_year":
+                    y = tag.attrib["content"]
+                if tag.attrib["name"] == "publication_month":
+                    m = tag.attrib["content"]
+                    if len(m) < 2:
+                        m = "0" + m
+                if tag.attrib["name"] == "publication_day_of_month":
+                    d = tag.attrib["content"]
+                    if len(d) < 2:
+                        d = "0" + d
+            ymd = "-".join([y, m, d])
+            if len(ymd) == 10:
+                result.append(ymd)
+            else:
+                result.append("")
+
         yield result
 
 # apply the regex
@@ -107,9 +132,11 @@ def gen_files(paths):
         elif os.path.isdir(path):
             # read all files in the directory
             for fname in os.listdir(path):
-                fname = os.path.join(path, fname)
-                if os.path.isfile(fname):
-                    yield open(fname, "rt"), fname
+                # restrict to XML files
+                if os.path.splitext(fname)[1] == ".xml":
+                    fname = os.path.join(path, fname)
+                    if os.path.isfile(fname):
+                        yield open(fname, "rt"), fname
         else:
             # read TAR file
             tar = tarfile.open(path, "r:gz")
@@ -125,12 +152,13 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Reading the NYT corpus.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('input', type=str, help='input TAR/XML file or directory', nargs='+')
-    parser.add_argument('-t', '--title', action="store_true", help='extract title')
+    parser.add_argument('-a', '--author', action="store_true", help='extract author')
     parser.add_argument('-b', '--body', action="store_true", help='extract body text')
-    parser.add_argument('-u', '--url', action="store_true", help='extract URL')
     parser.add_argument('-c', '--category', action="store_true", help='extract category')
     parser.add_argument('-d', '--desk', action="store_true", help='extract desk')
-    parser.add_argument('-a', '--author', action="store_true", help='extract author')
+    parser.add_argument('-e', '--date', action="store_true", help='extract date')
+    parser.add_argument('-t', '--title', action="store_true", help='extract title')
+    parser.add_argument('-u', '--url', action="store_true", help='extract URL')
     parser.add_argument('-g', '--grep', type=str, metavar="REGEX", help='match regex')
     parser.add_argument('-s', '--separator', type=str, metavar="SEP", help='output column separator', default='\t')
     parser.add_argument('-n', '--normalise-ws', action="store_true", help='normalise whitespace')
@@ -139,7 +167,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     files = gen_files(args.input)
-    parts = gen_parts(files, args.title, args.body, args.url, args.category, args.desk, args.author)
+    parts = gen_parts(files, args.title, args.body, args.url, args.category, args.desk, args.author, args.date)
     if args.grep:
         parts = gen_grep(parts, args.grep)
     if args.normalise_ws:
