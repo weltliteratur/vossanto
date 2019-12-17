18d52ee4c698f3cb203ba0b93b746eaf681a3aa3
Robert Jäschke <jaeschke@l3s.de>
2019-12-17 10:50:55 +0100

    removed images (they are broken!)

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -109,8 +109,8 @@ function htmlize(s, url) {
 // creates info bubble for an event
 function createInfo(e) {
     let image = "";
-    if (e.sImId) image += "<a href='https://commons.wikimedia.org/wiki/File:" + e.sImId + "'>" +
-	"<img src='https://upload.wikimedia.org/wikipedia/commons/" + e.sImTh + "'/></a>";
+//    if (e.sImId) image += "<a href='https://commons.wikimedia.org/wiki/File:" + e.sImId + "'>" +
+//	"<img src='https://upload.wikimedia.org/wikipedia/commons/" + e.sImTh + "'/></a>";
 
     console.log(e.sImId);
     console.log(e.sImTh);
