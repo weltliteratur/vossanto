5bafa6ce55626277398c29f2dfab447ae181fa34
schwabmi <michel.j.schwab@gmail.com>
2022-12-02 11:35:02 +0100

    improved functionality

--- dhd2023/scripte/modifier_vis.js
+++ dhd2023/scripte/modifier_vis.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
 const margin = {top: 10, right: 30, bottom: 30, left: 60}, width = 2000 - margin.left - margin.right,
-    height = 1000 - margin.top - margin.bottom;
+    height = 1500 - margin.top - margin.bottom;
 
 // // append the svg object to the body of the page
 const svg = d3.select("#visualization_container")
@@ -57,45 +57,53 @@ var color = d3.scaleOrdinal(d3.schemeCategory10);
 
 
 function visualize(data, infos, reduction) {
-    // transform datatype of x and y coordinates to numeric
-    data.forEach(function (d) {
-        d[reduction + "_x"] = +d[reduction + "_x"];
-    });
-    data.forEach(function (d) {
-        d[reduction + "_y"] = +d[reduction + "_y"];
-    });
 
-    // Add X axis
+    // X axis
     var x = d3.scaleLinear()
-        .domain(d3.extent(data, function (d) {
-            return d[reduction + "_x"];
-        }))
-        .range([0, width]);
-
-    // Add Y axis
+        .range([0, width])
+    // Y axis
     var y = d3.scaleLinear()
-        .domain(d3.extent(data, function (d) {
-            return d[reduction + "_y"];
-        }))
-        .range([height, 0]);
+        .range([height, 0])
 
+    // Using brush
     var brush = d3.brush().extent([[0, 0], [width, height]]).on("end", brushended), idleTimeout, idleDelay = 350;
 
+    // // add plot
+    // var scattertext = svg.append("g")
+    //     .attr("id", "scatterplot")
+    //     .attr("visualization_container-path", "url(#visualization_container)");
 
-    x.domain(d3.extent(data, function (d) {
-        return d[reduction + "_x"];
-    })).nice();
-    y.domain(d3.extent(data, function (d) {
-        return d[reduction + "_y"];
-    })).nice();
+    // add barchart
+    var bar_infos = infos.total_count
+    var bar_chart = d3.select("#bar_chart")
+        .append("g")
+        .attr("transform", "translate(" + 50 + "," + 0 + ")");
+
+    var bar_x = d3.scaleBand()
+        .range([0, 400])
+        .domain(bar_infos["8"])
+        .padding(0.2);
+    var xAxis = bar_chart.append("g")
+        .attr("transform", "translate(0," + 200 + ")")
+        .call(d3.axisBottom(bar_x).tickSize(0))
+        .selectAll("text")
+        .style("font", "0 px")
+
+    var bar_y = d3.scaleLinear()
+        .domain([0, Math.max.apply(null, bar_infos["8"])])
+        .range([200, 0])
+    var yAxis = bar_chart.append("g")
+        .call(d3.axisLeft(bar_y));
+
+    // legend
+    var legend = d3.select("#legend")
 
 
     var scattertext = svg.append("g")
         .attr("id", "scatterplot")
-        .attr("visualization_container-path", "url(#visualization_container)");
 
 
-    // "draw" labels
+    // draw plot
     scattertext.selectAll(".dot")
         .data(data)
         .enter().append("text")
@@ -124,94 +132,65 @@ function visualize(data, infos, reduction) {
             return color(d[9])
         })
 
-    bar_infos = infos.total_count
-    var bar_chart = d3.select("#bar_chart")
-        // .append("svg")
-        // .attr("width", 600)
-        // .attr("height", 400)
-        .append("g")
-        .attr("transform",
-            "translate(" + 50 + "," + 0 + ")");
 
-    var bar_x = d3.scaleBand()
-        .range([0, 400])
-        .domain(bar_infos["8"])
-        .padding(0.2);
-    var xAxis = bar_chart.append("g")
-        .attr("transform", "translate(0," + 200 + ")")
-        .call(d3.axisBottom(bar_x).tickSize(0))
-        .selectAll("text")
-        .style("font", "0 px")
+    // update plot depended on reduction algorithm
+    update_reduction(reduction, 25)
+
+    //update plot
+    function update_reduction(reduction, k) {
+
+        // computes new domains when brushed
 
 
-    // Add Y axis
-    var bar_y = d3.scaleLinear()
-        .domain([0, Math.max.apply(null, bar_infos["8"])])
-        .range([200, 0])
+        // update domains
+        x.domain(d3.extent(data, function (d) {
+            return d[reduction + "_x"];
+        })).nice()
 
+        y.domain(d3.extent(data, function (d) {
+            return d[reduction + "_y"];
+        })).nice()
 
-    var yAxis = bar_chart.append("g")
-        .call(d3.axisLeft(bar_y));
-//
-// Bars
-
-
-    var bars = bar_chart.selectAll("rect")
-        .data(bar_infos["8"])
-
-    bars
-        .enter()
-        .append("rect")
-        .attr("class", "bar")
-        .attr("x", function (d) {
-            return bar_x(d);
-        })
-        .attr("y", function (d) {
-            return bar_y(d);
-        })
-        .attr("width", bar_x.bandwidth())
-        .attr("height", function (d) {
-            return 200 - bar_y(d);
-        })
-        .attr("fill", function (d, i) {
-            return color(i)
-        })
-
-    bars.exit().remove()
-
-
-    var legend = d3.select("#legend")
-
-    keys = Array.from({length: slider.value()}, (x, i) => i)
-    legend.selectAll("mydots")
-        .data(keys)
-        .enter()
-        .append("circle")
-        .attr("cx", function (d, i) {
-            return 30 + i * 38
-        })
-        .attr("cy", 100) // 100 is where the first dot appears.
-        .attr("r", 15)
-        .style("fill", function (d, i) {
-            return color(i)
-        })
-
-        .on("click", function (d, i) {
-            d3.selectAll(".dot").style("fill", "rgb(0,0,0,0.1)")
-            for (let t of d3.selectAll(".dot")) {
-                slider_value = Math.round(slider.value())
-                color_p = color(t.__data__[slider_value])
-                if (t.__data__[slider_value] == i) {
-                    t.style.fill = color_p
+        // "update" coords of phrases
+        d3.selectAll(".dot")
+            .data(data)
+            .transition().duration(2000)
+            .attr("x", function (d) {
+                return x(d[reduction + "_x"]);
+            })
+            .attr("y", function (d) {
+                return y(d[reduction + "_y"]);
+            })
+            .attr("size", function (d) {
+                return d.count;
+            })
+            .text(function (d) {
+                return d.label;
+            })
+            // .style("font", "5px times")
+            .style("font", function (d) {
+                if (d.count < 5) {
+                    return "5px times"
+                } else {
+                    return d.count + "px times"
                 }
-            }
-        })
+            })
+            .style("fill", function (d) {
+                return color(d[9])
+            })
 
-    // call brush on scattertext
-    scattertext.append("g")
-        .attr("class", "brush")
-        .call(brush);
+        scattertext.append("g")
+            .attr("class", "brush")
+            .call(brush);
 
+        update_slider(slider.value())
+
+
+        // call brush on scattertext
+        scattertext.append("g")
+            .attr("class", "brush")
+            .call(brush);
+    }
 
     function brushended(event, d) {
         var s = event.selection;
@@ -238,9 +217,8 @@ function visualize(data, infos, reduction) {
         idleTimeout = null;
     }
 
+    // zooms in when brushed
     function zoom(s) {
-
-
         var t = scattertext.transition().duration(750);
         if (s == null) {
             scattertext.selectAll("text").transition(t)
@@ -287,34 +265,34 @@ function visualize(data, infos, reduction) {
     }
 
 
-    // change color when changing slider
-    slider.on('onchange', function (val, d) {
+    // update when changing slider
+    slider.on('onchange', update_slider)
+
+    // updates colors, bar chart, legend
+    function update_slider(val) {
+        changeColor(val)
 
         bar_x
-            .domain(bar_infos[(val-1).toString()])
-            .padding(0.2)
-        ;
+            .domain(bar_infos[(val - 1).toString()])
+            .padding(0.2);
+
+        console.log(Math.max.apply(null, bar_infos[(val - 1).toString()]))
+        console.log(yAxis)
+        bar_y
+            .domain([0, Math.max.apply(null, bar_infos[(val - 1).toString()])])
+        yAxis
+            .transition().duration(750)
+            .call(d3.axisLeft(bar_y));
 
         bar_chart.append("g")
             .attr("transform", "translate(0," + 200 + ")")
             .call(d3.axisBottom(bar_x).tickSize(0))
             .selectAll("text")
             .style("font", "0 px")
-        // .attr("transform", "translate(-10,0)rotate(-45)")
-        // .style("text-anchor", "end");
-//
-// Add Y axis
-        bar_y
-            .domain([0, Math.max.apply(null, bar_infos[(val-1).toString()])])
-        yAxis
-            .transition().duration(750)
-            .call(d3.axisLeft(bar_y));
-
-// Bars
 
 
         var bars = bar_chart.selectAll("rect")
-            .data(bar_infos[(val-1).toString()])
+            .data(bar_infos[(val - 1).toString()])
 
         bars
             .enter()
@@ -339,8 +317,7 @@ function visualize(data, infos, reduction) {
 
         bars.exit().remove()
 
-        // d3.select('#value').text(val);
-        changeColor(val)
+
         legend.selectAll("circle").remove();
         legend.selectAll("mydots")
             .data(Array.from({length: val}, (x, i) => i))
@@ -365,42 +342,22 @@ function visualize(data, infos, reduction) {
                     }
                 }
             })
+    }
 
-    });
 
-    clicked = buttons.on("click", function () {
+    buttons.on("click", function () {
         buttons.style("background", "#e7e7e7");
         d3.select(this).style("background", "#B2D8D8");
         reduction_name = this.value// console.log(this.value)
-        // todo:mit transition().duration(1000) o.ä. punkte verschieben lassen, sieht halt cooler aus.
-        //  bräcuhte aber eine andere architektur: update coordinates instead of drawing new
-        svg.selectAll("*").remove()
-        bar_chart.selectAll("*").remove()
-        Promise.all([
-            d3.csv('./data/data.csv'),
-            d3.json('./data/infos.json')
-        ]).then(([data, infos]) => {
-                visualize(data, infos, "pca_tsne")
-            }
-        )
-        // d3.csv("./data/data.csv").then(function (data) {
-        //     visualize(data, reduction_name)
-        // })
+        // bar_chart.selectAll("*").remove()
+        update_reduction(reduction_name, 50)
     })
-
 }
 
-Promise.all([
-    d3.csv('./data/data.csv'),
-    d3.json('./data/infos.json')
-]).then(([data, infos]) => {
-        visualize(data, infos, "pca_tsne")
-    }
-)
-// d3.csv("./data/data.csv").then(function (data) {
-//     visualize(data, "pca_tsne")
-// })
-
+// call function
+Promise.all([d3.csv('./data/data.csv'), d3.json('./data/infos.json')]).then(([data, infos]) => {
+    visualize(data, infos, "pca_tsne")
+})
 
 
 
